6a8a5241ccc510b674f8aec49b855dc783e968e4
Andrii Obukhivskyi <andreyobuh@gmail.com>
2025-10-05 16:01:18 +0300

    Add files via upload

---
+++ mad_dictator_data.js
@@ -0,0 +1,32 @@
+// mad_dictator_data.js
+export const madDictatorData = {
+    "UA": {
+        "Божевільний Диктатор": [
+            "👑 Шановний громадянине [NAME], офіційно підтверджуємо зарахування [AMOUNT] до цільового фонду функціонування моєї Імперії. Ваш внесок незамінний. 📜\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "📜 Документується: отримано фінансовий транш від громадянина [NAME]. Сума [AMOUNT] внесена до Золотого Реєстру № 451. Висловлюємо подяку за підкорення. 👑\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "👑 [NAME]! Ваш [AMOUNT] пройшов перевірку на лояльність. Відтепер Ви класифіковані як \"Суб'єкт Вищої Фінансової Цінності\". Не смійте зменшувати цей показник! 📜\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "📜 Протокол № 14: отримано [AMOUNT] від [NAME]. Це мінімальна вимога, але зафіксовано. Очікуємо на масштабну мобілізацію коштів від Вас у майбутньому. Виконайте! 👑\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "👑 [NAME], Ваш фінансовий звіт у розмірі [AMOUNT] затверджено! Я дозволяю Вам тимчасово насолоджуватися Вашою свободою. Дякуйте! 📜\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "📜 Нагадування: Внесок [AMOUNT] від [NAME] занесено до списку. Диктатура вимагає більше ресурсів для глобального домінування. Почніть діяти відповідно до мого плану! 👑\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "👑 [AMOUNT] від [NAME]! Наказ: Занести цей факт до Особистої Справи Громадянина! Ви тимчасово отримуєте підвищення статусу. Але моє око все ще стежить! 📜\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "📜 Наказ! [NAME], я підписую отримання [AMOUNT]. Ця сума буде використана для зміцнення моєї влади. Ви допомогли долі людства! 👑\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "👑 Громадянине [NAME], [AMOUNT]! Я дозволяю Вам внести цей транш. Ваша лояльність буде оцінена... можливо, колись. Слава мені! 📜\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+            "📜 [NAME], Ваш фінансовий документ у сумі [AMOUNT] пройшов аудит. Ви не викликаєте підозр. Наразі. Дякую за тимчасову покірність. 👑\nЗ повагою Ваш Божевільний Диктатор [CREATORNAME].",
+        ]
+    },
+    "EN": {
+        // ВИПРАВЛЕНО: Ключ для EN тепер 'The Mad Dictator'
+        "The Mad Dictator": [
+            "👑 Esteemed Citizen [NAME], we officially confirm the enrollment of [AMOUNT] into the dedicated fund for the operation of my Empire. Your contribution is irreplaceable. 📜\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "📜 Documented: Financial tranche received from Citizen [NAME]. The sum [AMOUNT] has been entered into Golden Register No. 451. We express gratitude for your submission. 👑\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "👑 [NAME]! Your [AMOUNT] has passed the Loyalty Check. You are henceforth classified as a \"Subject of Supreme Financial Value.\" Do not dare to diminish this metric! 📜\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "📜 Protocol No. 14: [AMOUNT] has been received from [NAME]. This is a minimum requirement, but it is documented. We expect a massive mobilization of funds from you in the future. Execute! 👑\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "👑 [NAME], your financial report in the amount of [AMOUNT] is Approved! I hereby grant you temporary enjoyment of your freedom. Be Thankful! 📜\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "📜 Reminder: The contribution of [AMOUNT] from [NAME] is filed. The Dictatorship requires more resources for global domination. Start acting according to My Plan! 👑\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "👑 [AMOUNT] from [NAME]! Order: Enter this fact into the Citizen's Personal File! You are temporarily granted elevated status. But My Eye is still watching! 📜\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "📜 Order! [NAME], I hereby sign off on receiving [AMOUNT]. This sum will be used to strengthen My authority. You have aided the destiny of mankind! 👑\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "👑 Citizen [NAME], [AMOUNT]! I permit you to submit this tranche. Your loyalty will be assessed... perhaps... one day. Glory to Me! 📜\nSincerely, Your Mad Dictator [CREATORNAME].",
+            "📜 [NAME], your financial document in the amount of [AMOUNT] has passed audit. You do not arouse suspicion. For now. Thank you for your temporary obedience. 👑\nSincerely, Your Mad Dictator [CREATORNAME].",
+        ]
+    }
+};
