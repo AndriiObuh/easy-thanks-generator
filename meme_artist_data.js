121e482f4e3df7b094a7f6ccc97e8b572add902b
Andrii Obukhivskyi <andreyobuh@gmail.com>
2025-10-05 16:01:50 +0300

    Add files via upload

---
+++ meme_artist_data.js
@@ -0,0 +1,31 @@
+// meme_artist_data.js
+export const memeArtistData = {
+    "UA": {
+        "Художник-Мемер": [
+            "🔥 [NAME]! Ти – просто легенда! Твої [AMOUNT] – це сабскрайб найвищого рівня! Дякую, тепер я можу ще тиждень жити на цьому вайбі! 😎\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "😎 Це легітно! Я не вірив, що ти це зробиш, [NAME]. Твої [AMOUNT] офіційно визнані як мій краш цього місяця. Greatest Of All Time! 🔥\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "🔥 Стоп! [NAME], ти реально закинув [AMOUNT]? Ти що, GOAT? Дякую, це імба! Тепер я офіційно зобов'язаний тобі чимось епічним. 😎\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "😎 Це не донат, [NAME]. Це інвестиція в мій новий мемний контент. Твої [AMOUNT] будуть використані на епічні рофли. Звіт буде в наступному відео! 🔥\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "🔥 [NAME]! [AMOUNT]! Оце так поворот! Ти щойно змусив мене зробити стрим-танець. Дякую, ти буквально забустив мій рівень енергії до 1000%! 😎\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "😎 [NAME] щойно перейшов на новий рівень. Ці [AMOUNT] — це чит-код! Ти, здається, забув, що це PREMIUM-контент! Дякую, це файно (Nice!). 🔥\nЗ по повагою Ваш Художник-Мемер [CREATORNAME].",
+            "🔥 [AMOUNT] від [NAME]! Це не сон! Я не знаю, що це означає, але я відчуваю, що ти підписав пакт зі мною. Дякую за апгрейд. LEVEL UP! 😎\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "😎 Увага! [NAME] щойно задокументовано як MVP (Most Valuable Patron). Твої [AMOUNT] увійдуть в історію! Рофл, але дякую! 🔥\nЗ по повагою Ваш Художник-Мемер [CREATORNAME].",
+            "🔥 Я тут щойно збентежився (крінж). Ти, [NAME], занадто щедрий! [AMOUNT] – це занадто багато для мого скромного сетапу. Але я прийму! 😎\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+            "😎 [NAME], ти знаєш, що ці [AMOUNT] означають? Це означає, що ми в грі! Це повний ресет мого настрою. Ти геній. Дякую! 🔥\nЗ повагою Ваш Художник-Мемер [CREATORNAME].",
+        ]
+    },
+    "EN": {
+        "The Meme Artist": [
+            "🔥 [NAME]! You are simply a legend! Your [AMOUNT] is the highest-level subscribe! Thanks, now I can live on this vibe for another week! 😎\nSincerely, Your Meme Artist [CREATORNAME].",
+            "😎 That’s legit! I didn't believe you'd do it, [NAME]. Your [AMOUNT] is officially recognized as my crush this month. Greatest Of All Time (GOAT)! 🔥\nSincerely, Your Meme Artist [CREATORNAME].",
+            "🔥 Stop! [NAME], did you really drop [AMOUNT]? Are you the GOAT? Thanks, that’s imba (overpowered)! Now I officially owe you something epic. 😎\nSincerely, Your Meme Artist [CREATORNAME].",
+            "😎 This isn't a donation, [NAME]. It's an investment in my new meme content. Your [AMOUNT] will be used for epic rofls (lmao moments). The report will be in the next video! 🔥\nSincerely, Your Meme Artist [CREATORNAME].",
+            "🔥 [NAME]! [AMOUNT]! What a plot twist! You just made me do a stream dance. Thanks, you literally boosted my energy level to 1000%! 😎\nSincerely, Your Meme Artist [CREATORNAME].",
+            "😎 [NAME] just went to the next level. This [AMOUNT] is a cheat code! You seem to have forgotten this is PREMIUM content! Thanks, that's nice (No cap). 🔥\nSincerely, Your Meme Artist [CREATORNAME].",
+            "🔥 [AMOUNT] from [NAME]! This is not a dream! I don't know what it means, but I feel like you've signed a pact with me. Thanks for the upgrade. LEVEL UP! 😎\nSincerely, Your Meme Artist [CREATORNAME].",
+            "😎 Attention! [NAME] is officially documented as the MVP (Most Valuable Patron). Your [AMOUNT] will go down in history! Rofl (lmao), but thank you! 🔥\nSincerely, Your Meme Artist [CREATORNAME].",
+            "🔥 I just caught the cringe here. [NAME], you're too generous! [AMOUNT] is too much for my humble setup. But I'll accept! 😎\nSincerely, Your Meme Artist [CREATORNAME].",
+            "😎 [NAME], do you know what this [AMOUNT] means? It means we are in the game! It's a full reset of my mood. You are a genius. Thanks! 🔥\nSincerely, Your Meme Artist [CREATORNAME].",
+        ]
+    }
+};
